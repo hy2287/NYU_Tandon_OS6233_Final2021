a712ff6e6f6a77d04541954a515bce177348e3f4
hy2287 <hy2287@nyu.edu>
2021-12-01 10:29:24 -0500

    Allow user to input blocksize for filesize testing

--- test.c
+++ test.c
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-void myWrite(int fd, int blockSize, int blockCount) {
+void myWrite(int fd, int blockSize, int blockCount, int randomized) {
     char* buffer;
     buffer = (char*) malloc(blockSize);
     for (int i = 0; i < blockCount; i++) {
-        for (int i = 0; i < blockSize; i++) {
-            buffer[i] = (rand() % 26) + 'a';
+        if(randomized){
+            for (int i = 0; i < blockSize; i++) {
+                buffer[i] = (rand() % 26) + 'a';
+            }
         }
         write(fd, buffer, blockSize);
     }
@@ -42,7 +44,7 @@ double findFileSize(int blockSize){
     while(timeNeeded<5){
         blockCount*=2;
         fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXO | S_IRWXG | S_IRWXU);
-        myWrite(fd,blockSize*blockCount,1);
+        myWrite(fd,blockSize*blockCount,1,0);
         close(fd);
 
         fd = open(filename, O_RDONLY);
@@ -61,29 +63,35 @@ double findFileSize(int blockSize){
 }
 
 int main(int argc, char *argv[]) {
-    int fd, blockSize, blockCount;
+    int fd, blockSize, blockCount, testBlockSize;
     char mode = 'x';
     unsigned int xorAnswer = 0;
 
-    sscanf (argv[2],"-%c", &mode);
-    sscanf (argv[3],"%d", &blockSize);
-    sscanf (argv[4],"%d", &blockCount);
+    if(argc==5){
+        sscanf (argv[2],"-%c", &mode);
+        sscanf (argv[3],"%d", &blockSize);
+        sscanf (argv[4],"%d", &blockCount);
+    }
 
     srand(time(0));
 
     if (mode == 'w' || mode == 'W') {
         fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, S_IRWXO | S_IRWXG | S_IRWXU);
-        myWrite(fd, blockSize, blockCount);
+        myWrite(fd, blockSize, blockCount,1);
         close(fd);
     }
     else if (mode == 'r' || mode == 'R'){
         fd = open(argv[1], O_RDONLY);
         xorAnswer = myRead(fd, blockSize);
-        printf("XOR Answer is %d", xorAnswer);
+        printf("XOR Answer is %d\n", xorAnswer);
         close(fd);
     }
-    else {
-        findFileSize(20);
+    else if (argc==1){
+        if(sscanf (argv[1],"%d", &testBlockSize)==0){
+            printf("Invalid arg provided\n.");
+        }
+        printf("block size = %d\n", testBlockSize);
+        findFileSize(testBlockSize);
     }
 
     exit(0);
